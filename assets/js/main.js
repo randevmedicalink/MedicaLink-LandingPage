e57d7bac71f21b67877c8509a48816cb677961ff
chirangashalitha <chiranga.20244008@iit.ac.lk>
2024-11-27 22:29:31 +0530

    Fix open and close button on sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,10 @@
 const sideBarCloseButton = document.getElementById("close-btn");
 const openMenuButton = document.getElementById("menu-btn");
-const bannercloseButton = document.getElementById("dismiss-btn");
 
-bannercloseButton.addEventListener("click", (e) => {
-  document.getElementById("blue-banner").classList.add("hidden");
+openMenuButton.addEventListener("click", (e) => {
+  document.getElementById("sidebar").classList.remove("hidden");
 });
 
 sideBarCloseButton.addEventListener("click", (e) => {
   document.getElementById("sidebar").classList.add("hidden");
-});
-
-openMenuButton.addEventListener("click", (e) => {
-  document.getElementById("sidebar").classList.remove("hidden");
 });
